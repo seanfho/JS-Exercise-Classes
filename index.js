b8ae7e05cd3e8c449468fe96b30caa45d52aa0f6
Sean Ho <seanfho@gmail.com>
2021-03-29 23:50:24 -0400

    Solution to Task2 added to index.js

--- index.js
+++ index.js
@@ -77,8 +77,27 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon){
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    fill(gallons){
+      return this.tank = this.tank + gallons;
+    }
+    drive(distance){
+      const maxDistance = this.tank * this.milesPerGallon;
+      if(distance < maxDistance){
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - distance / this.milesPerGallon;
+    }else{
+      this.odometer = this.odometer + maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles`
+    }
   }
+}
   
   /*
     TASK 3
